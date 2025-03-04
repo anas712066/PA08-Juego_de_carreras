9667a55f393960684110bf93cc886df182e23a98
Alebald234 <a24josbalesp@inspedralbes.cat>
2025-03-04 16:52:25 +0100

    Sumar y cambiar valor dinero total

--- js/funcions.js
+++ js/funcions.js
@@ -4,19 +4,47 @@ document.querySelector("#btnSum50").addEventListener("click", sumar50);
 document.querySelector("#btnRes10").addEventListener("click", resta10);
 document.querySelector("#btnRes50").addEventListener("click", resta50);
 
-let contador = 1000;
+let contador = 0;
+let dineroTotal = 1000;
+
 
 function sumar10(){
 
-    contador = contador + 10;
-    document.querySelector("#dineroApuesta").innerHTML = contador
+    if (dineroTotal == 0 || dineroTotal < 0){    
+        contador = contador + 0;
+        dineroTotal = 0;
+        document.querySelector("#dineroApuesta").innerHTML = contador
+        document.querySelector("#dineroTotal").innerHTML = dineroTotal
+    }
+
+    else{
+        /*Aumentar dinero apuesta*/
+        contador = contador + 10;
+        /*Disminuir dinero Total*/
+        dineroTotal = dineroTotal -10;
+        document.querySelector("#dineroApuesta").innerHTML = contador
+        document.querySelector("#dineroTotal").innerHTML = dineroTotal
+    }
 
 }
 
 function sumar50(){
 
-    contador = contador + 50;
-    document.querySelector("#dineroApuesta").innerHTML = contador
+    if (dineroTotal == 0 || dineroTotal < 0){    
+        contador = contador + 0;
+        dineroTotal = 0;
+        document.querySelector("#dineroApuesta").innerHTML = contador
+        document.querySelector("#dineroTotal").innerHTML = dineroTotal
+    }
+
+    else{
+        /*Aumentar dinero apuesta*/
+        contador = contador + 50;
+        /*Disminuir dinero Total*/
+        dineroTotal = dineroTotal -50;
+        document.querySelector("#dineroApuesta").innerHTML = contador
+        document.querySelector("#dineroTotal").innerHTML = dineroTotal
+    }
 
 }
 
@@ -30,6 +58,9 @@ function resta10(){
         document.querySelector("#dineroApuesta").innerHTML = contador
     }
 
+    dineroTotal = dineroTotal +10;
+    document.querySelector("#dineroTotal").innerHTML = dineroTotal;
+
 }
 
 function resta50(){
@@ -41,6 +72,9 @@ function resta50(){
         contador = 0;
         document.querySelector("#dineroApuesta").innerHTML = contador
     }
+
+    dineroTotal = dineroTotal +50;
+    document.querySelector("#dineroTotal").innerHTML = dineroTotal;
 
 }
 
